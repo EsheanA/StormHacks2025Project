b52008deb57a362956dff84408015d4c97227b83
Kenneth Pamintuan <kelp@Kenneths-MacBook-Air-2.local>
2025-10-04 12:35:55 -0700

    ggs im on the bus

--- skibidiRizzlers/src/components/Player.jsx
+++ skibidiRizzlers/src/components/Player.jsx
@@ -1,8 +1,12 @@
+import { Bounds } from "@react-three/drei"
+
 export function Player() {
     return (
-        <mesh position={[0, 0, 10]}>   {/* makes it so that its initialized 10 units along z axis */}
-            <boxGeometry args={[15, 15, 20]} /> 
-            <meshLambertMaterial color={0xffffff} flatShading /> 
-        </mesh>
+        <Bounds fit clip observe margin={10}> {/* so that the user is framed nicely in the camera */}
+            <mesh position={[0, 0, 10]}>   {/* makes it so that its initialized 10 units along z axis */}
+                <boxGeometry args={[15, 15, 20]} /> 
+                <meshLambertMaterial color={0xffffff} flatShading /> 
+            </mesh>
+        </Bounds>
     );
 }
