0d18149c7f79f8ca3a094039d620c85ef3eb9a32
Kenneth Pamintuan <kelp@Kenneths-MacBook-Air-2.local>
2025-10-04 11:52:56 -0700

    first commit lmfao

---
+++ skibidiRizzlers/src/Game.jsx
@@ -0,0 +1,10 @@
+import { Scene } from "./compoennts/Scene";
+import { Player } from "./components/Player";
+
+export default function Game() {
+    return (
+        <Scene> 
+            <Player />
+        </Scene>
+    )
+}
